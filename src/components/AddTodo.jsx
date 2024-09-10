494dbb0496c6f50af385a28f09bc3c82e36c1b28
aditya <ingenious.adig@gmail.com>
2024-09-10 20:54:45 +0530

    Initial Commit

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,15 +1,20 @@
+import styles from "./AddTodo.module.css";
+
 function AddTodo() {
   return (
     <div className="container text-center">
       <div className="row">
         <div className="col-6">
-          <input type="text" placeholder="Enter Todo Here" />
+          <input type="text" placeholder="Enter Todo Here"/>
         </div>
         <div className="col-4">
           <input type="date" />
         </div>
         <div className="col-2">
-          <button type="button" className="btn btn-success">
+          <button
+            type="button"
+            className={`"btn btn-success" ${styles.button}`}
+          >
             Add
           </button>
         </div>
